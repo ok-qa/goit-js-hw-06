483067fedb071f2b812b999c8a8632e8799ebd1f
ok-qa <olechka.kr@gmail.com>
2023-06-16 02:22:30 -0700

    task 09 done

--- js/task-09.js
+++ js/task-09.js
@@ -1,10 +1,15 @@
 const colorBtn = document.querySelector("button");
+const colorOutput = document.querySelector(".color");
 
 const changeColor = () => {
   const getRandomHexColor = Math.floor(Math.random() * 0xffffff).toString(16);
 
   document.body.style.background = `#${getRandomHexColor}`;
   console.log("click on button, got color", getRandomHexColor);
+
+  colorOutput.textContent = getRandomHexColor;
 };
 
 colorBtn.addEventListener("click", changeColor);
+
+
